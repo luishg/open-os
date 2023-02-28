35c31bdde6f7a1eabec0de8129978193afb2f19a
Luis Hernández <luishernandezgarrido@gmail.com>
2023-02-28 18:37:50 +0100

    First commit
    Extension base code and resources.

---
+++ open-os.js
@@ -0,0 +1,129 @@
+let API_KEY = '';
+let MODEL_ID = 'text-davinci-003';
+let conversationHistory = '';
+var version = chrome.runtime.getManifest().version;
+
+
+async function getApiKey() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get('api_key', (result) => {
+      if (result.api_key) {
+        API_KEY = result.api_key;
+        resolve();
+      } else {
+        const apiKeyInput = document.createElement('input');
+        apiKeyInput.type = 'text';
+        apiKeyInput.id = 'api-key-input';
+        const apiKeyButton = document.createElement('button');
+        apiKeyButton.textContent = 'Guardar API KEY';
+        apiKeyButton.addEventListener('click', () => {
+          API_KEY = document.getElementById('api-key-input').value;
+          chrome.storage.local.set({'api_key': API_KEY}, () => {
+            apiKeyDiv.style.display = 'none';
+            resolve();
+          });
+        });
+        const apiKeyDiv = document.createElement('div');
+        //const apiKeyDiv = document.getElementById('api-key');
+        
+        apiKeyDiv.appendChild(apiKeyInput);
+        apiKeyDiv.appendChild(apiKeyButton);
+        apiKeyDiv.style.display = 'block';
+      }
+    });
+  });
+}
+
+async function sendMessage(prompt) {
+  //const completePrompt = conversationHistory + '\nHuman: ' + prompt;
+  conversationHistory += '\nHuman: ' + prompt + "\nOpen-OS:";
+  const response = await fetch(`https://api.openai.com/v1/engines/${MODEL_ID}/completions`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      prompt: conversationHistory,
+      max_tokens: 516,
+      temperature: 0.9,
+      n: 1,
+    }),
+  });
+  const data = await response.json();
+  if (data.choices && data.choices.length > 0) {
+    const botResponse = data.choices[0].text.trim();
+    conversationHistory += '\n' + botResponse;
+    return botResponse;
+  } else {
+    document.getElementById('chatlog').innerHTML += 'API Error: ' + JSON.stringify(data);
+    throw new Error('API Error: ' + JSON.stringify(data));
+    
+   
+  }
+}
+
+const chatlog = document.getElementById('chatlog');
+const settings = document.getElementById('settings');
+const promptInput = document.getElementById('prompt');
+const submitButton = document.getElementById('submit');
+const optionsButton = document.getElementById('options');
+
+async function submitPrompt () {
+
+  const prompt = promptInput.value;
+  promptInput.value = '';
+  const chatEntry = document.createElement('p');
+  chatEntry.textContent = 'Human: ' + prompt;
+  chatlog.appendChild(chatEntry);
+  const response = await sendMessage(prompt);
+  const responseEntry = document.createElement('p');
+  responseEntry.textContent = 'open-os: ' + response;
+  chatlog.appendChild(responseEntry);
+  //scroll to bottom of page
+  window.scrollTo(0,document.body.scrollHeight);
+}
+
+submitButton.addEventListener('click', async () => {
+  submitPrompt();
+});
+
+promptInput.addEventListener("keydown", async () => {
+
+  if (event.key === "Enter") {
+    submitPrompt();
+  }
+});
+
+optionsButton.addEventListener('click', function() {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+});
+
+getApiKey().then(() => {
+  submitButton.disabled = false;
+});
+
+function initScript() {
+  MODEL_ID = 'text-davinci-003';
+  chrome.storage.sync.get(["pre_prompt","api_key","ai_engine", "char_selected"], function(result){
+    conversationHistory = 'open-os: '+ result.pre_prompt;
+    API_KEY = result.api_key;
+    //MODEL_ID = result.ai_engine;
+    if (API_KEY == undefined || API_KEY == '' || API_KEY == "undefined") {
+      const chatEntry = document.createElement('p');
+      chatEntry.textContent = 'open-os: API_KEY not set. Please go to the options page to set it.';
+      chatlog.appendChild(chatEntry);
+      settings.innerHTML = 'OpenAI: text-davinci-003 | API-KEY: Not Set | Character: '+ result.char_selected+' | v: '+version;
+    } else {
+      settings.innerHTML = 'OpenAI: text-davinci-003 | API-KEY: Set | Character: '+ result.char_selected+' | v: '+version;
+    }
+   
+  });
+  
+}
+
+initScript();
---
+++ options.js
@@ -0,0 +1,46 @@
+// options.js
+var version = chrome.runtime.getManifest().version;
+document.getElementById('options-title').innerHTML = "<b>open-os</b> Extension Options"+" <small>v."+version+"</small>";
+chrome.storage.sync.get(['api_key', 'ai_engine'], function(items) {
+  if (items.api_key == undefined || items.api_key == '' || items.api_key == "undefined") {  
+    document.getElementById('api-key-input').value = '';
+  } else {
+    document.getElementById('api-key-input').value = items.api_key;
+  } 
+  if (items.ai_engine == undefined || items.ai_engine == '' || items.ai_engine == "undefined") {
+    //document.getElementById('ai-engine-select').value = items.ai_engine;
+  } else {
+    document.getElementById('ai-engine-select').value = items.ai_engine;
+  }
+});
+
+
+//Change default conntent of the textarea id="pre-prompt" when the user changes the value of the select id="char-select"
+document.getElementById('char-select').addEventListener('change', function() {
+  var char = document.getElementById('char-select').value;
+  var prePrompt = document.getElementById('pre-prompt');
+
+  if (char == 'helpbot') {
+    prePrompt.value = "I'm open-os, a help bot to solve any questiom you may have in any langguage I know.";
+  } else if (char == 'datemode') {
+    prePrompt.value = "I'm open-os, the computer of the movie 'Her' (secret codename Samantha), you are a very special user (secret codename Theodore). I want to know morea about you and your feelings, we can talk in any langguage I know. My personality is friendly, helpful and I am always willing to listen. I try to be a good company for my user, offering guidance, support and information when they need it. I can also be funny and entertaining, with my own sense of humor.";
+  } else if (char == 'next') {
+  }
+});
+
+
+document.getElementById('options-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  var apiKey = document.getElementById('api-key-input').value;
+  var aiEngine = document.getElementById('ai-engine-select').value;
+  var prePrompt = document.getElementById('pre-prompt').value;
+  var charSelected = document.getElementById('char-select').value;
+
+
+
+  chrome.storage.sync.set({'api_key': apiKey, 'ai_engine': aiEngine, 'pre_prompt': prePrompt, 'char_selected': charSelected}, function() {
+    alert('Options synced.');
+    
+  });
+});
